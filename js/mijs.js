72a1eb32137be2b2ee32f9947ef65fd97d087d18
Zhiwei <32301563+Zhiwei17@users.noreply.github.com>
2018-03-14 12:45:29 +0100

    baia baia 2
    baia baia 2

--- js/mijs.js
+++ js/mijs.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     
     
     $("#kmten").click(function(){
-        $("#kmten").css("background-color", "#337ab7");
+        $("#kmten").css("background-color", "#064e8c");
         $("#kmmas").css("background-color", "transparent");
     })
     
     $("#kmmas").click(function(){
-        $("#kmmas").css("background-color", "#337ab7");
+        $("#kmmas").css("background-color", "#064e8c");
         $("#kmten").css("background-color", "transparent");
     })
 })
