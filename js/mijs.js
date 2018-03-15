4a43b7153f12b9489df284fb15da35724c4e7a48
Alumnes <Alumnes@inf4.cesnuria>
2018-03-15 17:08:07 +0100

    H3 y H4
    zZzZz perezaaaaaaaaa

--- js/mijs.js
+++ js/mijs.js
@@ -11,4 +11,21 @@ $(document).ready(function () {
         $("#kmmas").css("background-color", "#064e8c");
         $("#kmten").css("background-color", "transparent");
     })
+    
+    $("#enviar").click(function(){
+       
+        var nombre = $("#formNombre").val();
+        var email = $("#formEmail").val();
+        
+        if(nombre == null || nombre == ""){
+            //alert("caca de vaca");
+            $("#nameError").css('display', 'block').animate({fontSize: '14px'}, "slow");
+            $("#formNombre").css({"border-color": "red", 
+             "border-width":"2px", 
+             "border-style":"solid"});
+        }else{
+            
+        }
+        
+    })
 })
