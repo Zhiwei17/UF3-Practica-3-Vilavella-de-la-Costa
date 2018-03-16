4417b5a4598d496d18da19565ca385407f272a26
Zhiwei <32301563+Zhiwei17@users.noreply.github.com>
2018-03-16 10:08:08 +0100

    mamamiaaaaa video control hide

--- js/mijs.js
+++ js/mijs.js
@@ -1,31 +1,93 @@
 // Documento JavaScript
 $(document).ready(function () {
-    
-    
-    $("#kmten").click(function(){
+
+
+    $("#kmten").click(function () {
         $("#kmten").css("background-color", "#064e8c");
         $("#kmmas").css("background-color", "transparent");
     })
-    
-    $("#kmmas").click(function(){
+
+    $("#kmmas").click(function () {
         $("#kmmas").css("background-color", "#064e8c");
         $("#kmten").css("background-color", "transparent");
     })
-    
-    $("#enviar").click(function(){
-       
+
+    $("#enviar").click(function () {
+
         var nombre = $("#formNombre").val();
         var email = $("#formEmail").val();
-        
-        if(nombre == null || nombre == ""){
+
+        if (nombre == null || nombre == "") {
             //alert("caca de vaca");
-            $("#nameError").css('display', 'block').animate({fontSize: '14px'}, "slow");
-            $("#formNombre").css({"border-color": "red", 
-             "border-width":"2px", 
-             "border-style":"solid"});
-        }else{
-            
+            $("#nameError").css('display', 'block').animate({
+                fontSize: '14px'
+            }, "slow");
+            $("#formNombre").css({
+                "border-color": "red",
+                "border-width": "2px",
+                "border-style": "solid"
+            });
         }
-        
+
+        if (email == null || email == "") {
+            //alert("caca de vaca");
+            $("#emailError").css('display', 'block').animate({
+                fontSize: '14px'
+            }, "fast");
+            $("#formEmail").css({
+                "border-color": "red",
+                "border-width": "2px",
+                "border-style": "solid"
+            });
+        }
+
+        var zulul = 0;
+
+        if (email != "") {
+
+
+
+
+            $("#emailError").css('display', 'none').animate({
+                fontSize: '14px'
+            }, "fast");
+            $("#formEmail").css({
+                "border-width": "0px",
+            });
+
+            zulul = zulul + 1;
+
+        }
+
+        if (nombre != "") {
+            $("#nameError").css('display', 'none').animate({
+                fontSize: '14px'
+            }, "fast");
+            $("#formNombre").css({
+                "border-width": "0px",
+            });
+            zulul = zulul + 1;
+        }
+
+        if (zulul == 2) {
+            window.location.replace("thank-you-page.html");
+        }
     })
+
+
+    //is this the posiblu snipet for the snipt asdaddaksdwpe??
+    var vids = $("video");
+    $.each(vids, function(){
+        
+        this.controls = false;
+
+        $(this).click(function(){
+            this.controls = true;
+        })
+        
+    });
+
+
+
+
 })
